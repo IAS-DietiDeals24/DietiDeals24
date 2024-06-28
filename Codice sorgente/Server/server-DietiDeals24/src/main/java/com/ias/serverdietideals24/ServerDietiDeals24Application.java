88311fd063cabba7b48ee60b8228fd1520409d93
Simone Scisciola <simone.scisciola03@gmail.com>
2024-06-29 00:12:09 +0200

    Inizializzato server REST API con Dev Environment

---
+++ Codice sorgente/Server/server-DietiDeals24/src/main/java/com/ias/serverdietideals24/ServerDietiDeals24Application.java
@@ -0,0 +1,13 @@
+package com.ias.serverdietideals24;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServerDietiDeals24Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServerDietiDeals24Application.class, args);
+	}
+
+}
