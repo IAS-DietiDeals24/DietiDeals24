ead48bd6a6826d777e0a3d8a9a406494ad6987e2
Andrea Lucchese <andrylook14@gmail.com>
2024-08-29 09:49:48 +0200

    Aggiunta plugin Sonarqube

--- source-code/Client/frontendandroid/build.gradle.kts
+++ source-code/Client/frontendandroid/build.gradle.kts
@@ -10,4 +10,13 @@ plugins {
     alias(libs.plugins.google.services) apply false
     alias(libs.plugins.google.firebase.crashlytics) apply false
     alias(libs.plugins.google.firebase.perf) apply false
+    alias(libs.plugins.sonarqube)
+}
+
+sonar {
+    properties {
+        property("sonar.host.url", "http://localhost:9000")
+        property("sonar.projectKey", "dietideals24-frontendandroid")
+        property("sonar.projectName", "dietideals24-frontendandroid")
+    }
 }
