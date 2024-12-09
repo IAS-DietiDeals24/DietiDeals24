326b7f0cd72a2c4557c00f4219c4ff107b078c41
Simone Scisciola <simone.scisciola03@gmail.com>
2024-12-10 00:07:30 +0100

    Update database

---
+++ source-code/Server/database/data.sql
@@ -0,0 +1,14 @@
+INSERT INTO dd24.categoria_asta(nome) VALUES
+                                          ('Libri'),
+                                          ('Fumetti e Manga'),
+                                          ('Musica'),
+                                          ('Film e Serie TV'),
+                                          ('Videogame e Console'),
+                                          ('Elettronica'),
+                                          ('Cibo e Bevande'),
+                                          ('Animali domestici'),
+                                          ('Cura del corpo e Beauty'),
+                                          ('Hobby e Sport'),
+                                          ('Vestiti e Indossabili'),
+                                          ('Veicoli'),
+                                          ('Altro');
